dfb8547696bc4a1033228400a42061c85efb2db1
ingodinho <siemens.ingo@googlemail.com>
2022-06-15 13:17:01 +0200

    dies das

--- src/pages/BeerDetails.js
+++ src/pages/BeerDetails.js
@@ -13,7 +13,7 @@ const BeerDetails = () => {
         .then(json => {
             setDetails(json);
         })
-    }, [id])
+    }, [id]);
 
     return (
         <section className='beer-details'>
@@ -29,7 +29,7 @@ const BeerDetails = () => {
                 <span>{details.attenuation_level}</span>
             </div>
             <p>{details.description}</p>
-            <Link to={'/beers'}><button className='beer-details__btn'><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 32 32"><path d="M 13.28125 6.78125 L 4.78125 15.28125 L 4.09375 16 L 4.78125 16.71875 L 13.28125 25.21875 L 14.71875 23.78125 L 7.9375 17 L 28 17 L 28 15 L 7.9375 15 L 14.71875 8.21875 Z"/></svg></button></Link>
+            {id !== 'random' ? <Link to={'/beers'}><button className='beer-details__btn'><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 32 32"><path d="M 13.28125 6.78125 L 4.78125 15.28125 L 4.09375 16 L 4.78125 16.71875 L 13.28125 25.21875 L 14.71875 23.78125 L 7.9375 17 L 28 17 L 28 15 L 7.9375 15 L 14.71875 8.21875 Z"/></svg></button></Link> : ''}            
         </section>
     )
 }
